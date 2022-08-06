5ea0013d5915591e04089444d32308444b7f8019
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2022-08-06 09:45:19 -0400

    useState

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,11 +1,20 @@
 import { Heading, VStack, Icon, useTheme } from 'native-base';
 import { Envelope, Key } from 'phosphor-react-native';
+import { useState } from 'react';
+import { Alert } from 'react-native';
 import Logo from '../assets/logo_primary.svg';
 import { Button } from '../components/Button';
 import { Input } from '../components/Input';
 
 export function SignIn() {
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+
   const { colors } = useTheme();
+
+  function handleSignIn() {
+    console.log(name, password);
+  }
 
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
@@ -20,14 +29,17 @@ export function SignIn() {
         InputLeftElement={
           <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
         }
+        onChangeText={setName}
       />
+
       <Input
         placeholder="Senha"
         InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
         secureTextEntry
+        onChangeText={setPassword}
       />
 
-      <Button title="Entrar" w="full" />
+      <Button title="Entrar" w="full" onPress={handleSignIn} />
     </VStack>
   );
 }
